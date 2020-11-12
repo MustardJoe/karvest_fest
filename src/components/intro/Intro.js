2f9e8395a16b7bfe378b5176cd44dc2f06f3ceef
Jonathan Forney <jonathan.forney@gmail.com>
2020-11-12 03:24:34 -0800

    coming along nicely...

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -3,15 +3,12 @@ import sayings from '../../sayingsData';
 
 const Intro = () => {
   let ourSaying = sayings.getRandom();
-  console.log(sayings.getRandom());
   return (
     <div className="Intro">
       <div className="Heres">
       As the old hippy saying goes:
       </div>
-    
       {ourSaying}
-      
     </div>
   )
 }
