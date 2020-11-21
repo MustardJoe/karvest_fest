c441c3cd3aa5856dfa4a6204704283e9c4a6b03d
Jonathan Forney <jonathan.forney@gmail.com>
2020-11-20 23:09:05 -0800

    pre-publish style and content tweaks

--- src/App.js
+++ src/App.js
@@ -11,7 +11,28 @@ function App() {
     <div className="App">
       <Header/>
       <ImageContainer />
-      <p className="Informo">Inform-o regarding current weed strains you may have recieved:</p>
+        <h3 className="Card-header">Inform-o regarding current weed strains you may have recieved:</h3>
+      <section className="Informo">
+          <p>Hi Friends!</p>
+          <p>
+            It thrills me to be able to share some of my recent cannabis harvest with you!
+            Cannabis cultivation is a personal passion of mine, and when I have a good
+            harvest I love to share with fellow cannabis enthusiasts in my community.
+            Each plant I grow receives hundreds of hours care to bring it through from
+            seed/cutting to flowering plant, to dried, cured and smokable bud.
+          </p>
+          <p>
+            Please Note, I personally enjoy the more energizing, elevating “power-high"
+            provided by sativa and sativa-dominant cultivars, so that’s what I’ve grown.
+          </p>
+          <p>
+            Here’s to making to the end of 2020, hopefully we can all gather together
+            again whenever we can all gather together again,
+          </p>
+          <p>
+            Jon
+          </p>
+      </section>
       <Card strain={weedData.Bruce_Banner}/>
       <Card strain={weedData.Sour_Deisel}/>
       <Card strain={weedData.Nightmare_Cookies}/>
