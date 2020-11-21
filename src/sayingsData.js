e5cb15bb0f5b5105c17c8e2ffe13dfcb7bb460b1
Jonathan Forney <jonathan.forney@gmail.com>
2020-11-20 20:24:34 -0800

    small changes to bodies of text on these files

--- src/sayingsData.js
+++ src/sayingsData.js
@@ -1,13 +1,14 @@
 const sayingsData = {
   sayings: [
-    '"A hit of keif in the morning makes a person as strong as a hundred camels in the courtyard."',
+    '"A hit of keef in the morning makes a person as strong as a hundred camels in the courtyard."',
     '"Ancient stoner wisdom asks us to remember that when the spirit of the flower fills us, we feel full with the flying flower godess"',
     '"Wear a red rainbow for cheer, and remember, a green rainbow helps the green goblin know who his friends are."',
     '"Technology is just a new means to achieve an old end. We are nothing more than trousered apes traveling at 2000 miles per hour."',
     '"If you go nosing around for cabages, and you end up 3 feet down the ol crows gizzard, ya best have brung yer galloshes."'
   ],
   getRandom: () => {
-    const randomMsg = sayingsData.sayings[Math.floor(Math.random() * (sayingsData.sayings.length))];
+    const randomMsg = sayingsData.sayings[Math.floor(Math.random()
+      * (sayingsData.sayings.length))];
     console.log('sayingsData', randomMsg);
     return randomMsg;   
   }
--- src/weedData.js
+++ src/weedData.js
@@ -1,48 +1,22 @@
 let weedData = {
   Nightmare_Cookies: {
     name: 'Nightmare Cookies',
-    blurb: `I didn’t think I would like this one as much as I did, it was
-    described as “a sativa dominant hybrid, with some indica effects that
-    mostly come in the form of couch lock.” I think of it as great 
-    “night-time sativa,” for me, the ideal send off to dream land. Sadly,
-    the total yield from this plant was rather low, and samples are
-    especially limited.`,
+    blurb: `I didn’t think I would like this one as much as I do. It was described as “a sativa dominant hybrid, with some indica effects that mostly come in the form of couch lock,” which is not exactly appealing-sounding to me. I think it turned out as a great “night-time” sativa. For me it’s the ideal sendoff to dreamland, although you may find it better suited to a slightly early time in the evening - paired with ‘un digestif’, perhaps, enjoyed as part of an end to your evening meal. Sadly, this plant was part of a failed experiment resulting in a low yield, and samples are especially limited.`,
     img: '',
   },
   Sour_Deisel: {
     name: 'Sour Deisel',
-    blurb: `A perennial classic, this plant provides a boost to mood and
-    energy, and a good headspace adjustment, without too much
-    mind-bendiness, and no lethargy. “Great day-time weed” is what most
-    folks say, and it’s true. Well suited to hiking, yoga, playing or
-    listening to music, or getting those fall leaves raked. Personally,
-    I find this to be a good socially stimulating strain, for
-    conversation or parties. Eventually we will get to have those again.`,
+    blurb: `A perennial classic, this plant provides a boost to mood and energy, providing a good headspace adjustment without too much mind-bendiness, and no lethargy. “Great day-time weed,” is what most folks say, and it’s true. Perfectly suited to hiking, yoga, playing music, getting those fall leaves raked, or any number of adventures. I find this to be a good socially stimulating strain, for conversation or parties. Eventually we will get to have those again. `,
     img: '',
   },
   Bruce_Banner: {
     name: 'Bruce Banner #3',
-    blurb: `This is a mentally energizing, and at the same time, 
-    also coriously a profoundly calming high. This is the most chill of any of
-    these strains, but it is still definitely not a “chill out” strain.
-    Many of the folks who got to try the last batch of this strain I grew
-    said they really enjoyed it, although it isn’t one of my personal
-    favorites. Probably best suited to video games, watching stuff and
-    similar activities. If you really just want to space out mentally,
-    this is a good choice.`,
+    blurb: `This is a mentally energizing, and at the same time curiously, a profoundly calming high. This is the most chill of any of the strains I’ve grown this summer, but it is still definitely not a “chill out” strain. Many of the folks who got to try the batch of this really enjoyed it, although it isn’t one of my personal favorites. Probably a good choice for many to help decompress or “shift gear” ofter work or similar activity. If you want to completely space out mentally, this is a good choice.`,
     img: '',
   },
   Magic_Medicine: {
     name: 'Magic Medicine',
-    blurb: `This is a very special strain (A hybrid of 2 Sativa landrace
-      strains, I’d be happy to tell you more about it for any curious folks),
-      and it packs a powerful punch in the brain-department, with a high
-      bordering on psychedelic.  I find this strain is great for creative
-      activities, helping to both inspire my imagination and energize my
-      artistic and musical faculties. This is easily one of my favorite
-      strains of cannabis I’ve ever enjoyed. However, if you aren’t a fan of
-      strong sativas, or especially if you are prone to weed induced paranoia,
-      this may not be the strain for you.`,
+    blurb: `A very special cultivar (it’s a hybrid of two sativa landrace strains, Panama Red and Malana Cream), packing a powerful punch-in-the-brain in the brain-department - the high borders on psychedelic. The fragrance boasts an unique and pleasing aroma, with notes of honeysuckle, melon, and an “old school spiciness”.  I find this strain is great for creative activities, helping to both inspire my imagination and energize my artistic faculties. Or use it for a mini vision quest. This is easily one of my favorite strains of cannabis I’ve ever enjoyed. (I’d be happy to expound upon the fascinating history and linage of the Magic Medicine cultivar to anybody curious to learn more.) It’s unlikely you’ve had something quite like this one before. If you have, it wasn’t recently. However, if you aren’t a fan of strong sativas, or especially if you are prone to weed induced paranoia, this may not be the strain for you.`,
     img: '',
   }
 
