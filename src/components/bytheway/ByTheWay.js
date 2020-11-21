2fce9ed7227aadbd4128fff0722884165290f87a
Jonathan Forney <jonathan.forney@gmail.com>
2020-11-20 22:02:13 -0800

    contact info and contact styling

--- src/components/bytheway/ByTheWay.js
+++ src/components/bytheway/ByTheWay.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const ByTheWay = () => {
   return (
-    <section>
-      <h4>Before You Leave...</h4>
-      <p>Do you work in tech? As it just so happens...</p>
+    <section className="Informo">
+      <h2 className="Pacifico">Before You Leave...</h2>
+      <p>Do you work in tech? As it just so happens I am currently
+        looking for my tech job. I'm a passionate full stack JavaScript
+        practitioner who’s also interested in Operations. I’m also a
+        wonderful person to work with, and I’d love to be your new
+        teammate!</p>
+        <div>
+          <p>Many paths of communication exist:</p>
+          <span className="LinksContainer">
+            <a className="ContactLinks"
+              href="https://www.jonforney.com">www.jonforney.com</a>
+            <a className="ContactLinks"
+              href="https://www.linkedin.com/in/jon-forney/">www.linkedin.com/in/jon-forney/
+              </a>
+            <a className="ContactLinks" 
+              href="https://twitter.com/GodzillaJonny">@godzillajonny</a>
+            <a className="ContactLinks" 
+              href="mailto:jonathan.forney@gmail.com">jonathan.forney@gmail.com</a>
+          </span>
+        </div>
     </section>
   )
 }
