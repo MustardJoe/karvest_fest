27e3708c57e9eb68e58b3aba2aaf355d30aca9a6
Jonathan Forney <jonathan.forney@gmail.com>
2021-02-27 02:43:13 -0800

    updates for winter 2020/21 grow

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <ImageContainer />
       <section className="Informo">
           <p>Hi Friends!</p>
-          <p>
+          {/* <p>
             It thrills me to be able to share some of my recent cannabis harvest with you!
             Cannabis cultivation is a personal passion of mine, and when I have a good
             harvest I love to share with fellow cannabis enthusiasts in my community.
@@ -27,16 +27,28 @@ function App() {
           <p>
             Here’s to more or less making it to the end of 2020, hopefully we can all gather together
             again someday, whenever we can all gather together again,
+          </p> */}
+          <p>
+          WOW, 2020 beat the absolute shit out of me, and I’m definitely still hurting. But I know, at least for me, it feels like the worst of the collective hardship we are facing is behind us. I hope you feel this way as well, at least when you are able to stop and take a moment to reflect.
+          </p>
+          <p>
+          I know not all of us made it to 2021 -  we lost far too many of our loved ones and other wonderful people -  but if you’re reading this, you have made it. I’m glad you’re still here. Let’s heal together.
+          </p>
+          <p>
+          Over the last several months I’ve been refining my grow methods, trying new equipment, learning new techniques, and gearing up for this still-new year. 2021 is already shaping up to be a better year than I’ve had in more than a few (both in terms of my horticultural efforts and other personal endeavors), and I hope that’s true for you too. Hope – it’s something I’m surprised to realize I have again. 
+          </p>
+          <p>
+          It is in the spirit of hopeful healing that I am proud to offer samples from my winter 2020-21 indoor cannabis cultivation efforts.
           </p>
           <p>
             Jon
           </p>
       </section>
+      <br></br>
         <h3 className="Card-header">Inform-o regarding weed strains you may have recieved:</h3>
-      <Card strain={weedData.Bruce_Banner}/>
-      <Card strain={weedData.Sour_Deisel}/>
-      <Card strain={weedData.Nightmare_Cookies}/>
-      <Card strain={weedData.Magic_Medicine}/>
+      <Card strain={weedData.Obama_Kush}/>
+      <Card strain={weedData.Magic_Medicine_2}/>
+      <Card strain={weedData.Laughing_Buddha}/>
       <Bytheway/>
       <Intro/>
     </div>
--- src/weedData.js
+++ src/weedData.js
@@ -1,4 +1,19 @@
 let weedData = {
+  Obama_Kush: {
+    name: 'Obama Kush',
+    blurb: 'One of the few indica-dominant strains I really enjoy, this strain is a modern classic, much like the plant’s namesake. I find Obama Kush offers a powerfully physical, relaxing body high, without sending me to insta-snooze land. And yet it still has definite, strong Sativa notes that come through in the shape of a thought-expanding introspectiveness. It’s an excellent strain for those who appreciate cannabis for both its medicinal/therapeutic qualities as well as recreational enjoyment. This is a great late-evening strain for me, but one I also like at other times as well, as the physical effects help to melt stress out of my body. Great for watching cartoons, hanging out by the fire with friends, or a long soak in the tub (my personal suggestion), this strain is likely to appeal to a variety of folks turning to cannabis for any number of reasons. Solid gold. This particular phenotype has produced incredible looking buds, so frosty and purple… absolutely gorgeous.',
+    img: '',
+  },
+  Magic_Medicine_2: {
+    name: 'Magic Medicine',
+    blurb: 'A plant so nice, I grew it twice! And I’m very glad I did. This is one of my favorite strains ever: a lesser known, newer introduction by a small-scale grower of rare landrace and landrace-hybrid cultivars that are specifically acclimated for outdoor growing in Pacific Northwest climates. Truly unique! Unfortunately the farmer who produced this strain suffered devastating property destruction and crop losses from the 2020 Oregon wildfires, and has dropped of the map. I managed to take a cutting of the plant I grew outdoors last summer, and so was able to grow the strain a second time. I absolutely love Magic Medicine and I hope the farmer can make a comeback over the next year or so. A 100% Sativa cross between Panama Red and the Malana cultivar, strongly psychedelic, as far as cannabis goes. Excellent strain for making art/music and other creative stuff. ',
+    img: '',
+  },
+  Laughing_Buddha: {
+    name: 'Laughing Buddha',
+    blurb: 'Still growing, look for more info here in the next few weeks.',
+    img: '',
+  },
   Nightmare_Cookies: {
     name: 'Nightmare Cookies',
     blurb: `I didn’t think I would like this one as much as I do. It was described as “a sativa dominant hybrid, with some indica effects that mostly come in the form of couch lock,” which is not exactly appealing-sounding to me. I think it turned out as a great “night-time” sativa. For me it’s the ideal sendoff to dreamland, although you may find it better suited to a slightly early time in the evening - paired with ‘un digestif’, perhaps, enjoyed as part of an end to your evening meal. Sadly, this plant was part of a failed experiment resulting in a low yield, and samples are especially limited.`,
