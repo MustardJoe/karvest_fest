88d53c07b65956262f6e913830079406caf3e747
Jonathan Forney <jonathan.forney@gmail.com>
2021-05-27 01:16:42 -0700

    new saying

--- src/sayingsData.js
+++ src/sayingsData.js
@@ -8,7 +8,8 @@ const sayingsData = {
     '"Technology is just a new means to achieve an old end. We are nothing more than trousered apes traveling through space at 50,000 miles per hour, shooting laser beams into each others’ eyes for reasons that have, disappointingly, less to do with our high-minded morals and philosophies, and more to do with the same reasons why our dogs are prone to sniff each others’ butts. Or, in my case, it’s my cat who likes sniffing butts.“',
     '"Black lives matter, transphobes suck, and we need to stop destroying the only Earth we have. Fuck power"',
     '"Black lives matter, transphobes suck, and we need to stop destroying the only Earth we have. Fuck power"',
-    '“I lived under this bridge for a while now and I do drugs and alcohol as part of my religion. If you want to do some graffiti here, that’s cool with me. Thanks for the cigarettes.“'
+    '“I lived under this bridge for a while now and I do drugs and alcohol as part of my religion. If you want to do some graffiti here, that’s cool with me. Thanks for the cigarettes.“',
+    '"I’m the original Must-Mayo-Stard-Anayse."'
   ],
   getRandom: () => {
     const randomMsg = sayingsData.sayings[Math.floor(Math.random()
